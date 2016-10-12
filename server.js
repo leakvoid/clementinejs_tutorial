b3417f037efc9aeca2cb3fde2ed906c7736ca7ef
leakvoid <leakvoid@gmail.com>
2016-10-12 22:59:44 +0300

    refactoring routes

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 'use strict'
 
 var express = require('express');
+var routes = require('./app/routes/index.js');
 
 var app = express();
 
-app.get('/', function(req, res) {
-    //res.send('Hello, node!');
-    res.sendFile(process.cwd() + '/index.html');
-});
+routes(app);
 
 app.listen(3000, function() {
     console.log('Listening on port 3000...');
