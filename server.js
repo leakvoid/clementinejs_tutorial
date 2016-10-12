13cadab47d2d7524664133869dd74627413d9cde
leakvoid <leakvoid@gmail.com>
2016-10-12 22:34:08 +0300

    simple node server

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict'
+
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+    //res.send('Hello, node!');
+    res.sendFile(process.cwd() + '/index.html');
+});
+
+app.listen(3000, function() {
+    console.log('Listening on port 3000...');
+});
